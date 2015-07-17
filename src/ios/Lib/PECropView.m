76c349911a7a9e9661c976c35f5ce729e25a41a3
Jeduan Cornejo <jeduan@gmail.com>
2015-07-17 11:50:46 -0500

    Updating PECropView to https://github.com/kishikawakatsumi/PEPhotoCropEditor/commit/47bd189247d96aa55d2fb20619e7a43113b3d97e

--- src/ios/Lib/PECropView.m
+++ src/ios/Lib/PECropView.m
@@ -11,9 +11,9 @@
 #import "UIImage+PECrop.h"
 
 static const CGFloat MarginTop = 37.0f;
-static const CGFloat MarginBottom = MarginTop;
+//static const CGFloat MarginBottom = MarginTop;
 static const CGFloat MarginLeft = 20.0f;
-static const CGFloat MarginRight = MarginLeft;
+//static const CGFloat MarginRight = MarginLeft;
 
 @interface PECropView () <UIScrollViewDelegate, UIGestureRecognizerDelegate, PECropRectViewDelegate>
 
@@ -76,6 +76,7 @@ static const CGFloat MarginRight = MarginLeft;
     
     UIRotationGestureRecognizer *rotationGestureRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotation:)];
     rotationGestureRecognizer.delegate = self;
+    _rotationGestureRecognizer = rotationGestureRecognizer;
     [self.scrollView addGestureRecognizer:rotationGestureRecognizer];
     
     self.cropRectView = [[PECropRectView alloc] init];
